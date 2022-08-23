ce605563f708acd2a1d5301fc088cafc0f8bc9bb
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-23 13:12:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Geoffrey is the champion!";
+newHeader.className = "victory";
